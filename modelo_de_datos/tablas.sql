6ca52a9412424fd613d4275b011ada5d68bc6985
801950 <801950@unizar.es>
2022-03-16 05:15:16 -0700

    modelo de datos relacional y tablas en sql

--- modelo_de_datos/tablas.sql
+++ modelo_de_datos/tablas.sql
@@ -1,12 +1,19 @@
 CREATE TABLE Usuario
 (
-   Nombre_de_usuario           CHAR(-1) PRIMARY KEY,
-   Correo_electronico          CHAR(-1)        NOT NULL,
-   Contrasena                  CHAR(-1)        NOT NULL,
-   Pais                        CHAR(-1)        NOT NULL,
-   Puntos                      NUMBER        NOT NULL,
-   Conectado                   BOOLEAN        NOT NULL,
-   Usuario_Nombre_de_usuario   CHAR(-1)        NOT NULL,
+   Nombre_de_usuario    CHAR(50) PRIMARY KEY,
+   Correo_electronico   CHAR(100)        NOT NULL,
+   Contrasena           CHAR(40)        NOT NULL,
+   Pais                 CHAR(50)        NOT NULL,
+   Puntos               NUMBER        NOT NULL,
+   Conectado            BOOLEAN        NOT NULL
+);
+
+CREATE TABLE Amigo
+(
+   Usuario_Nombre_de_usuario   CHAR(50),
+   Usuario_Nombre_de_usuario   CHAR(50),
+   PRIMARY KEY (Usuario_Nombre_de_usuario,Usuario_Nombre_de_usuario),
+   FOREIGN KEY (Usuario_Nombre_de_usuario) REFERENCES Usuario(Nombre_de_usuario),
    FOREIGN KEY (Usuario_Nombre_de_usuario) REFERENCES Usuario(Nombre_de_usuario)
 );
 
