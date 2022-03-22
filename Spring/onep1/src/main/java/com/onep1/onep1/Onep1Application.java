ef22cca2c9c30f8f3382947ae4b7f41229322cae
801950 <801950@unizar.es>
2022-03-22 04:46:15 -0700

    spring if

---
+++ Spring/onep1/src/main/java/com/onep1/onep1/Onep1Application.java
@@ -0,0 +1,13 @@
+package com.onep1.onep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onep1Application.class, args);
+	}
+
+}
---
+++ Spring/onep1/src/test/java/com/onep1/onep1/Onep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onep1.onep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
