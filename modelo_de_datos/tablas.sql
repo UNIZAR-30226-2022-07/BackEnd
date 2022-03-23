6c36f978fc8165abf66047fe0a81fd4b04a1f07c
801950 <801950@unizar.es>
2022-03-23 14:57:36 -0700

    hicimos mal la bd

--- modelo_de_datos/tablas.sql
+++ modelo_de_datos/tablas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Usuario
 (
-   Nombre_de_usuario    CHAR(50) PRIMARY KEY,
-   Correo_electronico   CHAR(60)        NOT NULL,
+   Nombre_de_usuario    CHAR(50) NOT NULL,
+   Correo_electronico   CHAR(60)        PRIMARY KEY,
    Contrasena           CHAR(30)        NOT NULL,
    Pais                 CHAR(70)        NOT NULL,
    Puntos               INTEGER        NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE Notificaciones
    valor                       CHAR(200), -- autoincrementado
    Usuario   CHAR(50),
    PRIMARY KEY (valor,Usuario),
-   FOREIGN KEY (Usuario) REFERENCES Usuario(Nombre_de_usuario)
+   FOREIGN KEY (Usuario) REFERENCES Usuario(Correo_electronico)
 );
 
 CREATE TABLE Amigo
@@ -21,7 +21,7 @@ CREATE TABLE Amigo
    Usuario   CHAR(50),
    Usuario2   CHAR(50),
    PRIMARY KEY (Usuario,Usuario2),
-   FOREIGN KEY (Usuario) REFERENCES Usuario(Nombre_de_usuario),
-   FOREIGN KEY (Usuario2) REFERENCES Usuario(Nombre_de_usuario)
+   FOREIGN KEY (Usuario) REFERENCES Usuario(Correo_electronico),
+   FOREIGN KEY (Usuario2) REFERENCES Usuario(Correo_electronico)
 );
 
