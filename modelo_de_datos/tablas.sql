7f3ffe52beaeac4e55d0cef908b8814eaa053368
saladito <cesarborjamoreno@gmail.com>
2022-03-25 10:30:22 +0100

    atributos minusculas por claridad

--- modelo_de_datos/tablas.sql
+++ modelo_de_datos/tablas.sql
@@ -1,18 +1,18 @@
 CREATE TABLE Usuario
 (
-   Nombre_de_usuario    CHAR(50) NOT NULL,
-   Correo_electronico   CHAR(60)        PRIMARY KEY,
-   Contrasena           CHAR(30)        NOT NULL,
-   Pais                 CHAR(70)        NOT NULL,
-   Puntos               INTEGER        NOT NULL
+   nombre_de_usuario    CHAR(50)    NOT NULL,
+   correo_electronico   CHAR(60)    PRIMARY KEY,
+   contrasena           CHAR(30)    NOT NULL,
+   pais                 CHAR(70)    NOT NULL,
+   puntos               INTEGER     NOT NULL
 );
 
 CREATE TABLE Amigo
 (
-   Usuario   CHAR(50),
-   Usuario2   CHAR(50),
-   PRIMARY KEY (Usuario,Usuario2),
-   FOREIGN KEY (Usuario) REFERENCES Usuario(Correo_electronico),
-   FOREIGN KEY (Usuario2) REFERENCES Usuario(Correo_electronico)
+   usuario1             CHAR(50),
+   usuario2             CHAR(50),
+   PRIMARY KEY (usuario1,usuario2),
+   FOREIGN KEY (usuario1) REFERENCES Usuario(correo_electronico),
+   FOREIGN KEY (usuario2) REFERENCES Usuario(correo_electronico)
 );
 
