03c57428da1cc36aa20b860b18b99e6470eb4f7f
801950 <801950@unizar.es>
2022-03-29 03:05:43 -0700

    nuevo modelo de datos

--- modelo_de_datos/tablas.sql
+++ modelo_de_datos/tablas.sql
@@ -1,16 +1,16 @@
 CREATE TABLE Usuario
 (
-   nombre_de_usuario    CHAR(50)    NOT NULL,
-   correo_electronico   CHAR(60)    PRIMARY KEY,
-   contrasena           CHAR(30)    NOT NULL,
-   pais                 CHAR(70)    NOT NULL,
+   nombre_de_usuario    VARCHAR(255)    NOT NULL,
+   correo_electronico   VARCHAR(255)    PRIMARY KEY,
+   contrasena           VARCHAR(255)    NOT NULL,
+   pais                 VARCHAR(255)    NOT NULL,
    puntos               INTEGER     NOT NULL
 );
 
 CREATE TABLE Amigo
 (
-   usuario1             CHAR(50),
-   usuario2             CHAR(50),
+   usuario1             VARCHAR(255),
+   usuario2             VARCHAR(255),
    PRIMARY KEY (usuario1,usuario2),
    FOREIGN KEY (usuario1) REFERENCES Usuario(correo_electronico),
    FOREIGN KEY (usuario2) REFERENCES Usuario(correo_electronico)
