ae36884386c845401e1a03776da01fbc8413fe81
801950 <801950@unizar.es>
2022-04-12 04:40:59 -0700

    Co-authored-by: César Borja <saladito@users.noreply.github.com>

--- modelo_de_datos/tablas.sql
+++ modelo_de_datos/tablas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Usuario
 (
-   nombre_de_usuario    VARCHAR(255)    NOT NULL,
-   correo_electronico   VARCHAR(255)    PRIMARY KEY,
+   nombre_de_usuario    VARCHAR(255)    PRIMARY KEY,
+   correo_electronico   VARCHAR(255)    NOT NULL,
    contrasena           VARCHAR(255)    NOT NULL,
    pais                 VARCHAR(255)    NOT NULL,
    puntos               INTEGER     NOT NULL
@@ -12,7 +12,7 @@ CREATE TABLE Amigo
    usuario1             VARCHAR(255),
    usuario2             VARCHAR(255),
    PRIMARY KEY (usuario1,usuario2),
-   FOREIGN KEY (usuario1) REFERENCES Usuario(correo_electronico),
-   FOREIGN KEY (usuario2) REFERENCES Usuario(correo_electronico)
+   FOREIGN KEY (usuario1) REFERENCES Usuario(nombre_de_usuario),
+   FOREIGN KEY (usuario2) REFERENCES Usuario(nombre_de_usuario)
 );
 
