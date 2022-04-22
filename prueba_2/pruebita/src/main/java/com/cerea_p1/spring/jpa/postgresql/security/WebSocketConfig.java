bd5ab875f180d537af3c1664b82530ea246b7b25
801950 <801950@unizar.es>
2022-04-22 14:49:50 -0700

    websockets

--- prueba_2/pruebita/src/main/java/com/cerea_p1/spring/jpa/postgresql/security/WebSocketConfig.java
+++ prueba_2/pruebita/src/main/java/com/cerea_p1/spring/jpa/postgresql/security/WebSocketConfig.java
@@ -4,20 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import com.cerea_p1.spring.jpa.postgresql.security.websocket.WebSocketHandler;
 
 
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+ 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(new WebSocketHandler(), "/web-socket");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/game");
+    }
+ 
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+     //   registry.addEndpoint("/broadcast");  // it is OK to leave it here
+        // registry.addEndpoint("/broadcast").withSockJS();
+        // custom heartbeat, every 60 sec
+        registry.addEndpoint("/onep1-game").withSockJS();
+        
     }
 }
