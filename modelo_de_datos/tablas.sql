e4d135d1d2869a4c42a0b33d87a0e5b558b33714
801950 <801950@unizar.es>
2022-04-28 00:18:48 -0700

    base de datos

--- modelo_de_datos/tablas.sql
+++ modelo_de_datos/tablas.sql
@@ -1,18 +1,26 @@
 CREATE TABLE Usuario
 (
-   nombre_de_usuario    CHAR(50)    NOT NULL,
-   correo_electronico   CHAR(60)    PRIMARY KEY,
-   contrasena           CHAR(30)    NOT NULL,
-   pais                 CHAR(70)    NOT NULL,
+   nombre_de_usuario    VARCHAR(255)    PRIMARY KEY,
+   correo_electronico   VARCHAR(255)    NOT NULL,
+   contrasena           VARCHAR(255)    NOT NULL,
+   pais                 VARCHAR(255)    NOT NULL,
    puntos               INTEGER     NOT NULL
 );
 
 CREATE TABLE Amigo
 (
-   usuario1             CHAR(50),
-   usuario2             CHAR(50),
+   usuario1             VARCHAR(255),
+   usuario2             VARCHAR(255),
    PRIMARY KEY (usuario1,usuario2),
-   FOREIGN KEY (usuario1) REFERENCES Usuario(correo_electronico),
-   FOREIGN KEY (usuario2) REFERENCES Usuario(correo_electronico)
+   FOREIGN KEY (usuario1) REFERENCES Usuario(nombre_de_usuario),
+   FOREIGN KEY (usuario2) REFERENCES Usuario(nombre_de_usuario)
 );
 
+CREATE TABLE Invitacion
+(
+   emisor               VARCHAR(255),
+   receptor             VARCHAR(255),
+   PRIMARY KEY (emisor,receptor),
+   FOREIGN KEY (emisor) REFERENCES Usuario(nombre_de_usuario),
+   FOREIGN KEY (receptor) REFERENCES Usuario(nombre_de_usuario)
+)
