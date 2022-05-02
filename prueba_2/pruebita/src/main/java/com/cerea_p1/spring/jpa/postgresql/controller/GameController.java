bc582b37399fd826d37fa4df71daac323860f65f
nerea <801950@unizar.es>
2022-05-02 02:28:45 -0700

    clean

--- prueba_2/pruebita/src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/GameController.java
+++ prueba_2/pruebita/src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/GameController.java
@@ -1,10 +1,8 @@
 package com.cerea_p1.spring.jpa.postgresql.controller;
 
-//import ex.com.challenge.dto.ConnectRequest;
 import com.cerea_p1.spring.jpa.postgresql.exception.GameException;
 import com.cerea_p1.spring.jpa.postgresql.model.game.*;
 import com.cerea_p1.spring.jpa.postgresql.payload.request.game.CreateGameRequest;
-import com.cerea_p1.spring.jpa.postgresql.payload.request.game.DisconnectRequest;
 import com.cerea_p1.spring.jpa.postgresql.security.services.GameService;
 
 import lombok.AllArgsConstructor;
@@ -14,9 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
